fd36cfd8124fcdb805a779821a9cc95962ecf992
lhdht <15229684090@163.com>
2024-04-12 09:17:26 +0800

    feature: 日志

---
+++ core/internal/gorm.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"gorm.io/gorm/logger"
+)
+
+type GormWriter struct {
+	logger.Writer
+	Logger *zap.Logger
+}
+
+// NewWriter writer 构造函数
+func NewWriter(w logger.Writer, s *zap.Logger) *GormWriter {
+	return &GormWriter{
+		Writer: w,
+		Logger: s,
+	}
+}
+
+// Printf 格式化打印日志
+func (w *GormWriter) Printf(message string, data ...interface{}) {
+	w.Logger.Info(fmt.Sprintf(message+"\n", data...))
+}
