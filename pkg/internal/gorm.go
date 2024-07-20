d491923f1cbf7ea440bd18334e9e22e0ed235d8e
lhdht <15229684090@163.com>
2024-07-20 12:46:54 +0800

    fix: 修改日志

--- pkg/internal/gorm.go
+++ pkg/internal/gorm.go
@@ -21,5 +21,5 @@ func NewWriter(w logger.Writer, s *zap.Logger) *GormWriter {
 
 // Printf 格式化打印日志
 func (w *GormWriter) Printf(message string, data ...interface{}) {
-	w.Logger.Info(fmt.Sprintf(message+"\n", data...))
+	w.Logger.Info(fmt.Sprintf(message, data...))
 }
