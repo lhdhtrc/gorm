d542bf178b7ab19a3f8237bff5e3dec886e623a8
lhdht <15229684090@163.com>
2024-10-06 14:13:18 +0800

    fix: 处理uuid更新

--- pkg/model.go
+++ pkg/model.go
@@ -32,6 +32,7 @@ type ConfigEntity struct {
 }
 
 type TableEntity struct {
+	gorm.Model
 	ID        uint           `json:"id" gorm:"primarykey"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
