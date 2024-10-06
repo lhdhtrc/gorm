01870d33ecc0ffea12042b0b341a2a7ca7a7c226
lhdht <15229684090@163.com>
2024-10-06 14:31:53 +0800

    fix: 废弃

--- pkg/model.go
+++ pkg/model.go
@@ -1,7 +1,6 @@
 package gorm
 
 import (
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
@@ -40,7 +39,7 @@ type TableEntity struct {
 }
 
 type TableUUIDEntity struct {
-	ID        uuid.UUID      `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primarykey;size:36;"`
+	ID        string         `json:"id" gorm:"primarykey;size:36;"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
