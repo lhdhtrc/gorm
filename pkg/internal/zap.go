52ca6a8e340c76fd4f91e2699baf55fb6580bf7e
lhdht <15229684090@163.com>
2024-10-10 21:31:28 +0800

    feature: 集成zap

---
+++ pkg/internal/zap.go
@@ -0,0 +1,23 @@
+package internal
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"gorm.io/gorm/logger"
+)
+
+type Writer struct {
+	writer logger.Writer
+	logger *zap.Logger
+}
+
+// NewWriter writer 构造函数
+func NewWriter(l *zap.Logger, w logger.Writer) *Writer {
+	return &Writer{logger: l, writer: w}
+}
+
+// Printf 格式化打印日志
+func (w *Writer) Printf(message string, data ...interface{}) {
+	zap.L().Info(fmt.Sprintf(message, data...))
+	w.writer.Printf(message, data)
+}
