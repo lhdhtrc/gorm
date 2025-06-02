6c87a147a5731bf9c490f9e5c07987fbead61783
lhdht <15229684090@163.com>
2025-06-02 13:57:56 +0800

    fix: 修改

--- pkg/uuid_v7.go
+++ pkg/uuid_v7.go
@@ -2,26 +2,49 @@ package gorm
 
 import (
 	"database/sql/driver"
-	"errors"
+	"fmt"
 	"github.com/google/uuid"
 )
 
 type UUID uuid.UUID
 
-func (u *UUID) Scan(value any) (err error) {
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New("not scan uuid")
+func (u *UUID) Scan(value any) error {
+	if value == nil {
+		return nil
 	}
-	parseByte, err := uuid.FromBytes(bytes)
-	*u = UUID(parseByte)
-	return
+
+	switch v := value.(type) {
+	case []byte:
+		id, err := uuid.FromBytes(v)
+		if err != nil {
+			return fmt.Errorf("invalid UUID bytes: %w", err)
+		}
+		*u = UUID(id)
+	case string:
+		id, err := uuid.Parse(v)
+		if err != nil {
+			return fmt.Errorf("invalid UUID string: %w", err)
+		}
+		*u = UUID(id)
+	default:
+		return fmt.Errorf("unsupported UUID format: %T", value)
+	}
+
+	return nil
 }
 
 func (u UUID) Value() (bytes driver.Value, err error) {
-	return uuid.UUID(u).MarshalBinary()
+	id := uuid.UUID(u)
+	if id != uuid.Nil {
+		return id.MarshalBinary(), nil
+	}
+	return nil, nil
 }
 
 func (u UUID) String() string {
-	return uuid.UUID(u).String()
+	id := uuid.UUID(u)
+	if id != uuid.Nil {
+		return id.String()
+	}
+	return ""
 }
