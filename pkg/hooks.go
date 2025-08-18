3a77b6279056c2fa0770e6282d16a32e74635713
lhdht <15229684090@163.com>
2025-08-18 20:48:59 +0800

    fix: 修改

--- pkg/hooks.go
+++ pkg/hooks.go
@@ -2,8 +2,8 @@ package gorm
 
 import "gorm.io/gorm"
 
-// UsePaging 使用分页
-func UsePaging(sql *gorm.DB, page, size uint64) {
+// WithPaging 使用分页
+func WithPaging(sql *gorm.DB, page, size uint64) {
 	if size < 5 {
 		size = 5
 	}
