4a5835d8a2d0c2deb2a5b9d48ab593eeee2d87f4
lhdht <15229684090@163.com>
2025-11-15 17:41:30 +0800

    perfect: hooks -> scope

---
+++ pkg/scope/pagination.go
@@ -0,0 +1,18 @@
+package scope
+
+import "gorm.io/gorm"
+
+// WithPagination 使用分页
+func WithPagination(page, size uint64) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if size < 5 {
+			size = 5
+		}
+
+		if size > 100 {
+			size = 100
+		}
+
+		return db.Offset(int((page - 1) * size)).Limit(int(size))
+	}
+}
