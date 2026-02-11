3d6ab3bd1ed3ce694eda1b9c9797cafb74ca95d4
lhdht <15229684090@163.com>
2026-02-11 11:14:35 +0800

    perfect: log console format

--- internal/logger.go
+++ internal/logger.go
@@ -45,27 +45,27 @@ type Config struct {
 func NewLogger(config Config, handle func(b []byte)) loger.Interface {
 	// 定义各级别日志输出模板（可选彩色）
 	var (
-		infoStr      = "[%s] [info] [Database:%s] %s %s"
-		warnStr      = "[%s] [warn] [Database:%s] %s %s"
-		errStr       = "[%s] [error] [Database:%s] %s %s"
-		traceStr     = "[%s] [info] [Database:%s] [Rows:%v] [Duration:%.3fms] %s\n%s"
-		traceWarnStr = "[%s] [warn] [Database:%s] [Rows:%v] [Duration:%.3fms] %s %s\n%s"
-		traceErrStr  = "[%s] [error] [Database:%s] [Rows:%v] [Duration:%.3fms] %s %s\n%s"
+		infoStr      = "[%s] [info] [Database:%s]\n[Path] %s\n[Statement] %s"
+		warnStr      = "[%s] [warn] [Database:%s]\n[Path] %s\n[Message] %s\n[Statement] %s"
+		errStr       = "[%s] [error] [Database:%s]\n[Path] %s\n[Error] %s\n[Statement] %s"
+		traceStr     = "[%s] [info] [Database:%s] [Rows:%v] [Duration:%.3fms]\n[Path] %s\n[Statement] %s"
+		traceWarnStr = "[%s] [warn] [Database:%s] [Rows:%v] [Duration:%.3fms]\n[Path] %s\n[Message] %s\n[Statement] %s"
+		traceErrStr  = "[%s] [error] [Database:%s] [Rows:%v] [Duration:%.3fms]\n[Path] %s\n[Error] %s\n[Statement] %s"
 	)
 
 	// 若开启彩色输出，则替换模板为 gorm logger 预置的 ANSI 颜色字符串
 	if config.Colorful {
 		// date, level, db, file, msg
-		infoStr = loger.BlueBold + "[%s] " + loger.BlueBold + "[info] " + loger.BlueBold + "[Database:%s] " + loger.Green + "%s " + loger.Reset + "%s"
-		warnStr = loger.YellowBold + "[%s] " + loger.YellowBold + "[warn] " + loger.BlueBold + "[Database:%s] " + loger.Green + "%s " + loger.Reset + "%s"
-		errStr = loger.RedBold + "[%s] " + loger.RedBold + "[error] " + loger.BlueBold + "[Database:%s] " + loger.Green + "%s " + loger.Reset + "%s"
+		infoStr = loger.BlueBold + "[%s] " + loger.BlueBold + "[info] " + loger.BlueBold + "[Database:%s]\n" + loger.Green + "[Path] %s\n" + loger.Reset + "[Statement] %s"
+		warnStr = loger.YellowBold + "[%s] " + loger.YellowBold + "[warn] " + loger.BlueBold + "[Database:%s]\n" + loger.Green + "[Path] %s\n" + loger.Yellow + "[Message] %s\n" + loger.Reset + "[Statement] %s"
+		errStr = loger.RedBold + "[%s] " + loger.RedBold + "[error] " + loger.BlueBold + "[Database:%s]\n" + loger.Green + "[Path] %s\n" + loger.Red + "[Error] %s\n" + loger.Reset + "[Statement] %s"
 
 		// date, level, db, rows, timer, file, sql
-		traceStr = loger.BlueBold + "[%s] " + loger.BlueBold + "[info] " + loger.BlueBold + "[Database:%s] " + loger.YellowBold + "[Rows:%v]" + loger.Green + " [Duration:%.3fms] " + loger.Green + "%s\n" + loger.Reset + "%s"
+		traceStr = loger.BlueBold + "[%s] " + loger.BlueBold + "[info] " + loger.BlueBold + "[Database:%s] " + loger.YellowBold + "[Rows:%v]" + loger.Green + " [Duration:%.3fms]\n" + loger.Green + "[Path] %s\n" + loger.Reset + "[Statement] %s"
 		// date, level, db, rows, timer, file, slowLog, sql
-		traceWarnStr = loger.YellowBold + "[%s] " + loger.YellowBold + "[warn] " + loger.BlueBold + "[Database:%s] " + loger.YellowBold + "[Rows:%v]" + loger.Yellow + " [Duration:%.3fms] " + loger.Green + "%s " + loger.Yellow + "%s\n" + loger.Reset + "%s"
+		traceWarnStr = loger.YellowBold + "[%s] " + loger.YellowBold + "[warn] " + loger.BlueBold + "[Database:%s] " + loger.YellowBold + "[Rows:%v]" + loger.Yellow + " [Duration:%.3fms]\n" + loger.Green + "[Path] %s\n" + loger.Yellow + "[Message] %s\n" + loger.Reset + "[Statement] %s"
 		// date, level, db, rows, timer, file, err, sql
-		traceErrStr = loger.RedBold + "[%s] " + loger.RedBold + "[error] " + loger.BlueBold + "[Database:%s] " + loger.YellowBold + "[Rows:%v]" + loger.Green + " [Duration:%.3fms] " + loger.Green + "%s " + loger.Red + "%s\n" + loger.Reset + "%s"
+		traceErrStr = loger.RedBold + "[%s] " + loger.RedBold + "[error] " + loger.BlueBold + "[Database:%s] " + loger.YellowBold + "[Rows:%v]" + loger.Green + " [Duration:%.3fms]\n" + loger.Green + "[Path] %s\n" + loger.Red + "[Error] %s\n" + loger.Reset + "[Statement] %s"
 	}
 
 	// 返回实现 loger.Interface 的 logger 实例
